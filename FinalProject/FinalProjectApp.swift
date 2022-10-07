3efd5c8870975a6d3eb11ace1472f8c2d380828d
eman0930 <emanswimmer@gmail.com>
2022-10-07 07:56:35 -0600

    pulled for Mr. H

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Leavitt, Ethan on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
